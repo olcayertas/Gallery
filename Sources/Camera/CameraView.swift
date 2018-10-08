f28a45b47c0d64ba921622c9c1564fb399c35971
Olcay Ertaş <olcayertas@gmail.com>
2018-10-08 14:53:06 +0300

    Added camera orientation parameter to Config.

--- Sources/Camera/CameraView.swift
+++ Sources/Camera/CameraView.swift
@@ -111,7 +111,7 @@ class CameraView: UIView, UIGestureRecognizerDelegate {
     let layer = AVCaptureVideoPreviewLayer(session: session)
     layer.autoreverses = true
     layer.videoGravity = .resizeAspectFill
-    layer.connection?.videoOrientation = Utils.videoOrientation()
+    layer.connection?.videoOrientation = Config.orientation
     
     self.layer.insertSublayer(layer, at: 0)
     layer.frame = self.layer.bounds
